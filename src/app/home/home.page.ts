a2a0694fe09cc5922ccf97d6a02baf250f2febab
Iker Gonzalez <nintzatekeen@gmail.com>
2024-02-13 18:29:38 +0100

    Avances en el diseño

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonGrid, IonRow, IonCol, IonList, IonItem } from '@ionic/angular/standalone';
+import { IonicModule } from '@ionic/angular'; 
+// import { IonHeader, IonToolbar, IonTitle, IonContent, IonGrid, IonRow, IonCol, IonList, IonItem, IonButton, IonIcon } from '@ionic/angular/standalone';
 import { BuscadorComponent } from '../components/buscador/buscador.component';
 import { Anime } from '../model/anime';
 import { AnimeComponent } from '../anime/anime.component';
@@ -10,13 +11,15 @@ import { CommonModule } from '@angular/common';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, BuscadorComponent, AnimeComponent, IonGrid, IonRow, IonCol, IonList, IonItem, CommonModule],
+  // imports: [IonHeader, IonToolbar, IonTitle, IonContent, BuscadorComponent, AnimeComponent, IonGrid, IonRow, IonCol, IonList, IonItem, IonButton, IonIcon, CommonModule],
+  imports: [ IonicModule, CommonModule, BuscadorComponent, AnimeComponent]
 })
 export class HomePage {
 
   animes: Anime[] = [];
 
-  constructor() {}
+  constructor() {
+  }
 
   ver(anime: Anime) {
     this.animes.push(anime)
